9dab3ba71d4f37ca991b6a9467df582b312f38ab
Jiayuan Ren <renjiayuan1314@gmail.com>
2019-01-03 17:25:28 -0800

    WindowSurface Android-O version

--- graphicstest/WindowSurface.cpp
+++ graphicstest/WindowSurface.cpp
@@ -11,6 +11,10 @@ WindowSurface::WindowSurface(int w, int h) {
 
 }
 
+// do it manually here
+#define ANDROID_O
+
+#if defined(ANDROID_P)
 WindowSurface::WindowSurface() {
     status_t err;
 
@@ -63,6 +67,73 @@ WindowSurface::WindowSurface() {
 
     mSurfaceControl = sc;
 }
+
+#elif defined(ANDROID_O)
+
+WindowSurface::WindowSurface() {
+    status_t err;
+
+    sp<SurfaceComposerClient> surfaceComposerClient = new SurfaceComposerClient;
+    err = surfaceComposerClient->initCheck();
+    if (err != NO_ERROR) {
+        fprintf(stderr, "SurfaceComposerClient::initCheck error: %#x\n", err);
+        return;
+    }
+
+    // Get main display parameters.
+    sp<IBinder> mainDpy = SurfaceComposerClient::getBuiltInDisplay(
+            ISurfaceComposer::eDisplayIdMain);
+    DisplayInfo mainDpyInfo;
+    err = SurfaceComposerClient::getDisplayInfo(mainDpy, &mainDpyInfo);
+    if (err != NO_ERROR) {
+        fprintf(stderr, "ERROR: unable to get display characteristics\n");
+        return;
+    }
+
+    // do not use the default one, use the one passed by user,
+    // the surf size should always be 640*480, fix this later.
+   uint32_t width, height;
+#if 1
+   width = 640;
+   height = 480;
+#else
+    uint32_t width, height;
+    if (mainDpyInfo.orientation != DISPLAY_ORIENTATION_0 &&
+            mainDpyInfo.orientation != DISPLAY_ORIENTATION_180) {
+        // rotated
+        width = mainDpyInfo.h;
+        height = mainDpyInfo.w;
+    } else {
+        width = mainDpyInfo.w;
+        height = mainDpyInfo.h;
+    }
+#endif
+
+    sp<SurfaceControl> sc = surfaceComposerClient->createSurface(
+            String8("Benchmark"), width, height,
+            PIXEL_FORMAT_RGBX_8888, ISurfaceComposerClient::eOpaque);
+    if (sc == NULL || !sc->isValid()) {
+        fprintf(stderr, "Failed to create SurfaceControl\n");
+        return;
+    }
+
+    SurfaceComposerClient::openGlobalTransaction();
+    err = sc->setLayer(0x7FFFFFFF);     // always on top
+    if (err != NO_ERROR) {
+        fprintf(stderr, "SurfaceComposer::setLayer error: %#x\n", err);
+        return;
+    }
+
+    err = sc->show();
+    if (err != NO_ERROR) {
+        fprintf(stderr, "SurfaceComposer::show error: %#x\n", err);
+        return;
+    }
+    SurfaceComposerClient::closeGlobalTransaction();
+
+    mSurfaceControl = sc;
+}
+#endif
 
 EGLNativeWindowType WindowSurface::getSurface() const {
     sp<ANativeWindow> anw = mSurfaceControl->getSurface();
